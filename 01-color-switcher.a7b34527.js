6f1e2f6046c67d92d5b00459d3a22a850346143a
OleksiiBiesieda <104728633+OleksiiBiesieda@users.noreply.github.com>
2022-10-23 12:09:59 +0000

    Deploying to gh-pages from @ OleksiiBiesieda/goit-js-hw-09@e56b28bf6bb18861e3579f4261a2ce4b6b3c1fa8 🚀

---
+++ 01-color-switcher.a7b34527.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let r=null;function n(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){r=setInterval(n,1e3),t.setAttribute("disabled","true"),e.removeAttribute("disabled","true")})),e.addEventListener("click",(function(){clearInterval(r),e.setAttribute("disabled","true"),t.removeAttribute("disabled","true")}));
+//# sourceMappingURL=01-color-switcher.a7b34527.js.map
